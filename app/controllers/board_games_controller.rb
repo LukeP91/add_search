717a0618469489467abee6b5f0c09e81bc057cf9
Luke Pawlik <lukepawlik@gmail.com>
2017-01-04 14:37:44 +0100

    add search functionality

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,7 +1,11 @@
 class BoardGamesController < ApplicationController
 
   def index
-    @board_games = BoardGame.all
+    if params[:search]
+      @board_games = BoardGame.search(params[:search])
+    else
+      @board_games = BoardGame.all
+    end
   end
 
   def show
--- app/models/board_game.rb
+++ app/models/board_game.rb
@@ -1,3 +1,7 @@
 class BoardGame < ActiveRecord::Base
   belongs_to :category
+
+  def self.search(search_params)
+    joins(:category).where("board_games.name ILIKE ? OR categories.name ILIKE ?", "%#{search_params}%", "%#{search_params}%")
+  end
 end
