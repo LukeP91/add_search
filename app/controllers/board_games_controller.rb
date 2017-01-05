5f0fa5019a1fa94fa9a0b51988b1508d933f0f61
Luke Pawlik <lukepawlik@gmail.com>
2017-01-05 15:58:25 +0100

    extracted search from #index to #search

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,14 +1,15 @@
 class BoardGamesController < ApplicationController
 
   def index
-    if params[:search]
-      @board_games = BoardGame.search(params[:search])
-    else
       @board_games = BoardGame.all
-    end
   end
 
   def show
     @board_game = BoardGame.find(params[:id])
+  end
+
+  def search
+    @board_games = BoardGame.search(params[:search])
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#home'
 
-  resources :board_games, only: [:show, :index]
+  resources :board_games, only: [:show, :index] do
+    collection do
+      get 'search'
+    end
+  end
   resources :categories, only: [:index]
 end
